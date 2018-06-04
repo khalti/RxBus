b1c8969db05ff27f52083eb1f06bccde4382bdfd
Ishwor Shrestha <ishwor@janakitech.com>
2018-06-04 14:51:06 +0545

    migrated to rxjava 2

--- app/src/main/java/com/rxbusimpl/MainActivity.java
+++ app/src/main/java/com/rxbusimpl/MainActivity.java
@@ -9,6 +9,7 @@ import com.rxbus.RxBus;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import io.reactivex.disposables.CompositeDisposable;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,19 +20,22 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.btnAdd2)
     Button btnAdd2;
 
+    CompositeDisposable compositeDisposable;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        RxBus.getInstance().register(event -> {
+        compositeDisposable = new CompositeDisposable();
+        compositeDisposable.add(RxBus.getInstance().register(event -> {
             if (event.getTag().equals("add_1")) {
                 txt.setText((String) event.getEvent());
             } else if (event.getTag().equals("add_2")) {
                 txt.setText((String) event.getEvent());
             }
-        });
+        }));
 
         btnAdd1.setOnClickListener(view -> RxBus.getInstance().post("add_1", (Integer.parseInt(txt.getText() + "") + 1) + ""));
         btnAdd2.setOnClickListener(view -> RxBus.getInstance().post("add_2", (Integer.parseInt(txt.getText() + "") + 2) + ""));
--- rxbus/src/main/java/com/rxbus/RxBus.java
+++ rxbus/src/main/java/com/rxbus/RxBus.java
@@ -1,20 +1,19 @@
 package com.rxbus;
 
-import rx.Subscription;
-import rx.functions.Action1;
-import rx.subjects.PublishSubject;
-import rx.subjects.SerializedSubject;
-import rx.subjects.Subject;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.subjects.PublishSubject;
+import io.reactivex.subjects.Subject;
 
 public class RxBus {
     private static final RxBus INSTANCE = new RxBus();
-    private final Subject<Object, Object> bus = new SerializedSubject<>(PublishSubject.create());
+    private final Subject<Object> bus = PublishSubject.create();
 
     public static RxBus getInstance() {
         return INSTANCE;
     }
 
-    public Subscription register(Action1<Event> onNext) {
+    public Disposable register(Consumer<Event> onNext) {
         return bus
                 .filter(event -> event.getClass().equals(Event.class))
                 .map(obj -> (Event) obj)
